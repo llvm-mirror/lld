llvm-mirror/lld

commit a6aa8106065e5ee13b9b3311dbe0fa06ac605f0e

Slightly simplify code and add comment.
This is not a mechanical transformation. Even though I believe this
patch is correct, I'm not 100% sure if lld with this patch behaves
exactly the same way as before on all edge cases. At least all tests
still pass.

I'm submitting this patch because it took almost a day to understand
this function, and I don't want to lose it.

git-svn-id: https://llvm.org/svn/llvm-project/lld/trunk@315658 91177308-0d34-0410-b5e6-96231b3b80d8

ELF/Relocations.cpp
@@ -815,13 +815,31 @@ static void addGotEntry(SymbolBody &Sym, bool Preemptible) {
     DynType = Target->GotRel;
   }
 
-  bool Constant = !Preemptible && (!Config->Pic || isAbsolute(Sym));
-  if (!Constant)
+  // If a GOT slot value can be calculated at link-time, which is now,
+  // we can just fill that out.
+  //
+  // (We don't actually write a value to a GOT slot right now, but we
+  // add a static relocation to a Relocations vector so that
+  // InputSection::relocate will do the work for us. We may be able
+  // to just write a value now, but it is a TODO.)
+  bool IsLinkTimeConstant = !Preemptible && (!Config->Pic || isAbsolute(Sym));
+  if (IsLinkTimeConstant) {
+    InX::Got->Relocations.push_back({Expr, DynType, Off, 0, &Sym});
+  } else {
+    // Otherwise, we emit a dynamic relocation to .rel[a].dyn so that
+    // the GOT slot will be fixed at load-time.
     In<ELFT>::RelaDyn->addReloc(
         {DynType, InX::Got, Off, !Preemptible, &Sym, 0});
 
-  if (Constant || (!Config->IsRela && !Preemptible))
-    InX::Got->Relocations.push_back({Expr, DynType, Off, 0, &Sym});
+    // REL type relocations don't have addend fields unlike RELAs, and
+    // their addends are stored to the section to which they are applied.
+    // So, store addends if we need to.
+    //
+    // This is ugly -- the difference between REL and RELA should be
+    // handled in a better way. It's a TODO.
+    if (!Config->IsRela)
+      InX::Got->Relocations.push_back({R_ABS, Target->GotRel, Off, 0, &Sym});
+  }
 }
 
 // The reason we have to do this early scan is as follows

commit 1174125f7504a732906761f8d5b66e6d186169ab

Don't write preemptible symbol values to the .got.
It is not necessary and matches what bfd and gold do.

This was a regression from r315658.

git-svn-id: https://llvm.org/svn/llvm-project/lld/trunk@321023 91177308-0d34-0410-b5e6-96231b3b80d8

ELF/Relocations.cpp
@@ -846,7 +846,7 @@ template <class ELFT> static void addGotEntry(Symbol &Sym, bool Preemptible) {
   //
   // This is ugly -- the difference between REL and RELA should be
   // handled in a better way. It's a TODO.
-  if (!Config->IsRela)
+  if (!Config->IsRela && !Preemptible)
     InX::Got->Relocations.push_back({R_ABS, Target->GotRel, Off, 0, &Sym});
 }
 

test/ELF/i386-got-value.s
@@ -1,10 +1,10 @@
 # RUN: llvm-mc %s -o %t.o -filetype=obj -triple=i386-pc-linux
 # RUN: ld.lld %t.o -o %t.so -shared
-# RUN: llvm-readobj --relocations --symbols --sections --section-data %t.so | FileCheck %s
+# RUN: llvm-readobj --relocations --sections --section-data %t.so | FileCheck %s
 
-# Check that the value of a preemptible symbol is written to the got
-# entry when using Elf_Rel. It is not clear why that is required, but
-# freebsd i386 seems to depend on it.
+# Check that the value of a preemptible symbol is not written to the
+# got entry when using Elf_Rel. It is not needed since the dynamic
+# linker will write the final value.
 
 # CHECK:      Name: .got
 # CHECK-NEXT: Type: SHT_PROGBITS
@@ -20,13 +20,10 @@
 # CHECK-NEXT: AddressAlignment:
 # CHECK-NEXT: EntrySize:
 # CHECK-NEXT: SectionData (
-# CHECK-NEXT:   0000: 00200000
+# CHECK-NEXT:   0000: 00000000
 # CHECK-NEXT: )
 
 # CHECK: R_386_GLOB_DAT bar 0x0
-
-# CHECK:      Name: bar
-# CHECK-NEXT: Value: 0x2000
 
         movl    bar@GOT(%eax), %eax
 